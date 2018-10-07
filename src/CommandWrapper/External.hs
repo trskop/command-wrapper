30a1e812d8c243b5845c0f35b0164ce4cd8b3827
Peter Trško <peter.trsko@gmail.com>
2018-10-07 22:02:06 +0100

    Introducing `findSubcommands` that lists all available external subcommands

--- src/CommandWrapper/External.hs
+++ src/CommandWrapper/External.hs
@@ -18,20 +18,24 @@ module CommandWrapper.External
     ( Command
     , run
     , executeCommand
+    , findSubcommands
     )
   where
 
 import Control.Applicative (pure)
-import Control.Monad ((>>=))
+import Control.Exception (onException)
+import Control.Monad ((>>=), filterM)
 import Data.Bool (Bool(False))
-import Data.Function ((.))
+import Data.Foldable (length)
+import Data.Function ((.), ($))
 import Data.Functor ((<$>), (<&>), fmap)
+import qualified Data.List as List (drop, isPrefixOf, nub)
 import Data.List.NonEmpty (NonEmpty)
 import qualified Data.List.NonEmpty as NonEmpty (toList)
-import Data.Maybe (Maybe(Just, Nothing), listToMaybe)
+import Data.Maybe (Maybe(Just, Nothing), catMaybes, listToMaybe)
 import Data.Semigroup ((<>))
 import Data.String (String)
-import Control.Exception (onException)
+import Data.Traversable (mapM)
 import System.Exit (die)
 import System.IO (FilePath, IO{-, print-})
 
@@ -39,16 +43,27 @@ import System.IO (FilePath, IO{-, print-})
 import qualified Mainplate (ExternalCommand(..))
 import System.Directory
     ( XdgDirectory(XdgConfig)
+    , doesDirectoryExist
+    , executable
     , findExecutablesInDirectories
+    , getPermissions
     , getXdgDirectory
+    , listDirectory
     )
-import System.FilePath ((</>), (<.>), getSearchPath)
+import System.FilePath ((</>), (<.>), takeFileName)
+import qualified System.FilePath as FilePath (getSearchPath)
 import System.Posix.Process (executeFile)
 
 import qualified CommandWrapper.Config as Global
     ( Config(Config, searchPath, verbosity)
     )
-import qualified CommandWrapper.Environment as Environment -- (Params{-(Params)-})
+import qualified CommandWrapper.Environment as Environment
+    ( AppNames(AppNames, exePath, names, usedName)
+    , Params(Params, config, exePath, name, verbosity)
+    , commandWrapperEnv
+    , getEnv
+    , mkEnvVars
+    )
 
 
 type Command = Mainplate.ExternalCommand
@@ -112,17 +127,61 @@ findSubcommandExecutable
     :: NonEmpty (String, String)
     -> Global.Config
     -> IO (Maybe (String, String, FilePath))
-findSubcommandExecutable subcommands Global.Config{Global.searchPath} = do
-    searchPath' <- (searchPath <>) <$> getSearchPath
+findSubcommandExecutable subcommands config = do
+    searchPath <- getSearchPath config
 --  print ("Search path", searchPath')
-    loop searchPath' (NonEmpty.toList subcommands)
+    loop searchPath (NonEmpty.toList subcommands)
   where
-    loop searchPath' = \case
+    loop searchPath = \case
         [] -> pure Nothing
         (prefix, subcommand) : untriedSubcommands ->
-            findSubcommandExecutable' searchPath' subcommand >>= \case
+            findSubcommandExecutable' searchPath subcommand >>= \case
                 r@(Just _) -> pure ((prefix, subcommand, ) <$> r)
-                Nothing -> loop searchPath' untriedSubcommands
+                Nothing -> loop searchPath untriedSubcommands
 
-    findSubcommandExecutable' searchPath' =
-        fmap listToMaybe . findExecutablesInDirectories searchPath'
+    findSubcommandExecutable' searchPath =
+        fmap listToMaybe . findExecutablesInDirectories searchPath
+
+-- | Find all (unique) external subcommands.
+findSubcommands :: Environment.AppNames -> Global.Config -> IO [String]
+findSubcommands Environment.AppNames{Environment.names} config = do
+    searchPath <- getSearchPath config
+    executablesToSubcommands <$> mapM listDirectoryExecutables searchPath
+  where
+    prefixes :: NonEmpty String
+    prefixes = (<> "-") <$> names
+
+    listDirectoryExecutables :: FilePath -> IO [FilePath]
+    listDirectoryExecutables dir = do
+        directoryExists <- doesDirectoryExist dir
+        if directoryExists
+            then listDirectory dir >>= filterM (`isExecutableIn` dir)
+            else pure []
+
+    isExecutableIn :: FilePath -> FilePath -> IO Bool
+    isExecutableIn file dir = executable <$> getPermissions (dir </> file)
+
+    executablesToSubcommands :: [[FilePath]] -> [String]
+    executablesToSubcommands =
+        List.nub . (>>= (>>= executableToSubcommand))
+
+    executableToSubcommand :: FilePath -> [String]
+    executableToSubcommand filePath =
+        catMaybes $ matchSubcommand fileName <$> NonEmpty.toList prefixes
+      where
+        fileName = takeFileName filePath
+
+    matchSubcommand :: String -> String -> Maybe String
+    matchSubcommand fileName prefix =
+        if prefix `List.isPrefixOf` fileName
+            then Just $ List.drop (length prefix) fileName
+            else Nothing
+
+-- | Get search path for subcommands, i.e. list of directories where (external)
+-- subcommand executables are expected to be present.
+getSearchPath :: Global.Config -> IO [FilePath]
+getSearchPath Global.Config{Global.searchPath} =
+    (searchPath <>) <$> FilePath.getSearchPath
+    -- Search path provided in configuration has precedence to system search
+    -- path (value of `PATH` environment variable). This allows users to
+    -- override external subcommands if necessary.
