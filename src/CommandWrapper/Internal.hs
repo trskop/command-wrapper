99e988ce8fc827da311a1799b27cae1b754cf3ed
Peter Trsko <peter.trsko@gmail.com>
2019-07-08 14:51:54 +0100

    Reports proper Dhall Standard version as part of `version` subcommand

--- src/CommandWrapper/Internal.hs
+++ src/CommandWrapper/Internal.hs
@@ -153,7 +153,7 @@ run globalHelp appNames = \case
         -- This is a hacky way how to print Dhall Standard that Dhall library
         -- is using.  Unfortunately there is no nicer alternative at the
         -- moment.
-        , dhallStandard = Subcommand.PrettyVersion (makeVersion [7, 0, 0])
+        , dhallStandard = Subcommand.PrettyVersion (makeVersion [8, 0, 0])
         }
 
     completionConfig = Subcommand.CompletionConfig
